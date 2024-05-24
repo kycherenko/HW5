85b09372d3726109e3a5076a8589f99166d83990
kuche <julia.ooo.w@gmail.com>
2024-05-24 19:32:24 +0500

    Внесены исправления

--- src/Main.java
+++ src/Main.java
@@ -18,15 +18,6 @@ public class Main {
             } else {
                 System.out.println( "iOS по ссылке");
             }
-            if (clientOS == 1 && clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            } else if (clientOS == 1 && clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else if (clientOS == 0 && clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            } else if (clientOS == 0 && clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
         }
 
         int year = 2021;
